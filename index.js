bd0b75fbd7e8995a9ab5085febcde497cb50a655
Kolawole Daniel <kolawolecoke@gmail.com>
2023-10-29 21:59:46 +0100

    fixed image bg position and fixed bug in overflow

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ hamburger.addEventListener("click", handleBurgerClick);
 function handleBurgerClick(e) {
   hamburger.classList.toggle("open");
   mobileNav.classList.toggle("open__nav");
-  if (e.target.classList.contains("open")) {
-    document.body.style.overflow = "clip";
+  if (e.currentTarget.classList.contains("open")) {
+    document.body.style.overflow = "hidden";
   } else {
     document.body.style.overflow = "scroll";
   }
