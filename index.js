5aba49614bca6f019ad43c8861338216ef62bf0f
Kolawole Daniel <kolawolecoke@gmail.com>
2023-10-29 20:29:54 +0100

    positioned movile links and active state of footer links

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile__navigation");
 hamburger.addEventListener("click", handleBurgerClick);
 
 function handleBurgerClick(e) {
   hamburger.classList.toggle("open");
+  mobileNav.classList.toggle("open__nav");
+  if (e.target.classList.contains("open")) {
+    document.body.style.overflow = "clip";
+  } else {
+    document.body.style.overflow = "scroll";
+  }
 }
